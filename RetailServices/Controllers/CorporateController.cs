11ee91c7bbabf98904f92682b5d48f8dafd9dc3e
kiyotaka.abe@tm-nets.com <kiyotaka.abe@tm-nets.com>
2021-04-26 13:03:53 +0900

    プロジェクト ファイルを追加します。

---
+++ RetailServices/Controllers/CorporateController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RetailServices.Controllers
+{
+    public class CorporateController : ControllerBase
+    {
+        HttpClient _client;
+        public CorporateController(HttpClient client)
+        {
+            _client = client;
+        }
+
+
+        [HttpPost("addcoporate")]
+        public async Task AddCoporate(string coporateCd)
+        {
+
+
+        }
+
+    }
+}
---
+++ RetailServices/Services/Cart/GrpcService/CartService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailServices.Services.Cart.GrpcService
+{
+    public class CartService
+    {
+        private readonly ILogger<CartService> _logger;
+
+        public  CartService(ILogger<CartService> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
